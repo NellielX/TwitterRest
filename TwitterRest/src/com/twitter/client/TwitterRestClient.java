80f687bfea6bd69a5461442b6bc63d58bb05b4c3
Gaëtan <gae.marzilli@gmail.com>
2015-10-23 15:14:45 +0200

    Clean Main

--- TwitterRest/src/com/twitter/client/TwitterRestClient.java
+++ TwitterRest/src/com/twitter/client/TwitterRestClient.java
@@ -14,45 +14,4 @@ public class TwitterRestClient {
 		};
 		SwingUtilities.invokeLater(r);
 	}
-	// public static void main(String[] args) {
-	//
-	// try {
-	//
-	// // create HTTP Client
-	// HttpClient httpClient = HttpClientBuilder.create().build();
-	//
-	// // Create new getRequest with below mentioned URL
-	// HttpGet getRequest = new
-	// HttpGet("http://localhost:8080/TwitterRest/ServiceTwitter/twitterservice/");
-	//
-	// // Add additional header to getRequest which accepts application/xml data
-	// getRequest.addHeader("accept", "application/xml");
-	//
-	// // Execute your request and catch response
-	// HttpResponse response = httpClient.execute(getRequest);
-	//
-	// // Check for HTTP response code: 200 = success
-	// if (response.getStatusLine().getStatusCode() != 200) {
-	// throw new RuntimeException("Failed : HTTP error code : " +
-	// response.getStatusLine().getStatusCode());
-	// }
-	//
-	// // Get-Capture Complete application/xml body response
-	// BufferedReader br = new BufferedReader(new
-	// InputStreamReader((response.getEntity().getContent())));
-	// String output;
-	// System.out.println("============Output:============");
-	//
-	// // Simply iterate through XML response and show on console.
-	// while ((output = br.readLine()) != null) {
-	// System.out.println(output);
-	// }
-	//
-	// } catch (ClientProtocolException e) {
-	// e.printStackTrace();
-	//
-	// } catch (IOException e) {
-	// e.printStackTrace();
-	// }
-	// }
 }
